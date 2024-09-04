8e203d58b7263c3f9502b009ff1340e5ffba93f4
Dante Vazquez <74936894+dantevazquez@users.noreply.github.com>
2024-09-04 14:44:13 -0700

    updated projects and resume

--- src/data/user.js
+++ src/data/user.js
@@ -24,29 +24,37 @@ const INFO = {
 	about: {
 		title: "Hi! My name is Dante Vazquez",
 		description:
-			"I am currently a fourth-year student at UC San Diego studying Urban Planning, but my goal is to transition into a career in Software Development. I've refined my coding skills by earning an Associate's Degree in Computer Science from Saddleback College and completing a coding bootcamp at Fullstack Academy. Additionally, I am actively working on personal projects to stay up-to-date with new technologies and tools.",
+			"I am currently a Equipment Maintenance Technician at Google via Astreya, but my goal is to transition into a career in Software Development. I've refined my coding skills by earning an Associate's Degree in Computer Science from Saddleback College and completing a coding bootcamp at Fullstack Academy. Additionally, I am actively working on personal projects to stay up-to-date with new technologies and tools.",
 
 		image: aboutImg,
 		resume: resume,
 	},
 
 	projects: [
+
+		{
+			title: "Pocket Surf",
+			description:
+				"Co-Developed a mobile surfing game for IOS and Android in C# using the game engine Unity",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/csharp/csharp.png",
+			linkText: "View Project",
+			link: "https://play.google.com/store/apps/details?id=com.DevsDevelop.PocketSurf",
+		},
+		{
+			title: "Swell Calculator",
+			description:
+				"Ocean wave/swell forecasting tool built with React and MaterialUI",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://swellcalculator.netlify.app",
+		},
 		{
 			title: "Rotten Potatoes",
 			description:
 				"A movie review website where a user is view and write reviews on a catalog of over 10,000 movies. Implemented the frontend of this project using React-JS and the backend using Postgresql and Express JS",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://movie-reviews-n8mv.onrender.com",
-		},
-
-		{
-			title: "Swell Calculator",
-			description:
-				"Created the frontend for a wave/swell forecast model used to assist forecasters. The forecaster input known data such as wind speed, fetch lengths and wind duration and is returned a model that can be used to determine the size of waves near shore",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://swellcalculator.netlify.app",
+			link: "https://github.com/dantevazquez/Movie-Reviews",
 		},
 
 		{
@@ -61,7 +69,7 @@ const INFO = {
 		{
 			title: "Personal Website",
 			description:
-				"You're already viewing it, but feel free to check out the code! I created the homescreen without the use of a game engine using only React",
+				"You're already viewing it, but feel free to check out the code!",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
 			link: "https://github.com/dantevazquez/personal-site",
@@ -73,12 +81,12 @@ const INFO = {
 				"Website where users are able to build pokemon decks and share them. This was a group project I helped make during my time at FullStack Academy. ",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://pokemon-deck-builder.onrender.com",
+			link: "https://github.com/dantevazquez/pokemon-react",
 		},
 		{
 			title: "Destroy the Core",
 			description:
-				"Minecraft Plugin I made a while back inorder to teach myself how to code. The plugin is a minigame where players compete to break a block aka the core. First player to break the core x amount of times wins the game. The game keeps track of how many times a user has broken the core and rewards the winner with in game items ",
+				"Minecraft mini-game plugin",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
 			linkText: "View Project",
 			link: "https://github.com/dantevazquez/DestroyTheCore",
